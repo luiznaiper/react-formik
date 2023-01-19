290b30ec03d34bbabf4d6637110b51668f840d99
luiznaiper <luiz_naiper@hotmail.com>
2023-01-19 06:56:23 -0300

    :construction: Finish formik little project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import Checkbox from './components/Checkbox';
+import Radio from './components/Radio';
+import Select from './components/Select';
+import TextInput from './components/TextInput';
 
 const validate = (values) => {
   const errors = {};
@@ -17,28 +21,52 @@ const validate = (values) => {
   } else if (values.email.length < 8) {
     errors.email = 'Min Length 8 characters';
   }
+  if (!values.accept || values.accept === 'false') {
+    errors.accept = 'Accept or die';
+  }
+  if (!values.pokemon || values.pokemon === '') {
+    errors.pokemon = 'Required';
+  }
+  if (!values.radio) {
+    errors.radio = 'Required';
+  }
   return errors;
 };
 
 function App() {
   return (
     <Formik
-      initialValues={{ name: '', lastName: '', email: '' }}
+      initialValues={{
+        name: '',
+        lastName: '',
+        email: '',
+        pokemon: '',
+        accepted: false,
+        radio: '',
+      }}
       validate={validate}
       onSubmit={(values) => console.log(values)}
     >
       <Form>
         <label>Name</label>
-        <Field type="text" name="name" />
-        <ErrorMessage name="name" />
+        <TextInput name="name" label="Your Name" />
         <br />
         <label>Last Name</label>
-        <Field type="text" name="lastName" />
+        <TextInput type="text" name="lastName" />
         <br />
-        <ErrorMessage name="lastName" />
         <label>Email</label>
-        <Field type="email" name="email" />
-        <ErrorMessage name="email" />
+        <TextInput type="email" name="email" />
+        <Select label="Pokemon type" name="pokemon">
+          <option value="">Select your pokemon</option>
+          <option value="water">Water</option>
+          <option value="fire">Fire</option>
+          <option value="cheese">Cheese</option>
+        </Select>
+        <Radio name="radio" value="male" label="male" />
+        <Radio name="radio" value="female" label="female" />
+        <Radio name="radio" value="shemale" label="shemale" />
+        <ErrorMessage name="radio" />
+        <Checkbox name="accept">Accept terms</Checkbox>
         <button type="submit">Send</button>
       </Form>
     </Formik>
